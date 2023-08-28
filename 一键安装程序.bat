af2cbe4da00f0e81e3cdcdf265e19c75f4ecb17c
xiaofeng xie <xiexiaofeng2019@gmail.com>
2023-08-28 13:25:07 +0800

    add 一键安装程序.bat

---
+++ 一键安装程序.bat
@@ -0,0 +1,5 @@
+python -m venv venv
+call venv\scripts\activate
+pip install -i https://mirrors.aliyun.com/pypi/simple/ tb-nightly
+python.exe -m pip install –upgrade pip
+pip install -r requirements.txt
